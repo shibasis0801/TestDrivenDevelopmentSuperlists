03955c83c0d1522fd5d988becb898b324019cc24
shibasis0801 <shibasis0801@gmail.com>
2019-03-04 14:54:01 +0530

    URL mapped to dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve 
 from lists.views import home_page
-
+from django.http import HttpRequest
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -9,3 +9,12 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
